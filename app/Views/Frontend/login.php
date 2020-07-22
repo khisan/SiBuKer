71b41ffd109f13266a916d20b0ed1289a2df1e78
khisan <khisan8@gmail.com>
2020-07-22 16:33:34 +0700

    rubah struktur folder

--- app/Views/Frontend/login.php
+++ app/Views/Frontend/login.php
@@ -1,21 +1,21 @@
 <!-- css daftar -->
 <!-- plugins:css -->
-<link rel="stylesheet" href="<?php echo base_url()?>assets/frontend/vendors/iconfonts/mdi/css/materialdesignicons.min.css">
-<link rel="stylesheet" href="<?php echo base_url()?>assets/frontend/vendors/iconfonts/ionicons/css/ionicons.css">
-<link rel="stylesheet" href="<?php echo base_url()?>assets/frontend/vendors/iconfonts/typicons/src/font/typicons.css">
-<link rel="stylesheet" href="<?php echo base_url()?>assets/frontend/vendors/iconfonts/flag-icon-css/css/flag-icon.min.css">
-<link rel="stylesheet" href="<?php echo base_url()?>assets/frontend/vendors/css/vendor.bundle.base.css">
-<link rel="stylesheet" href="<?php echo base_url()?>assets/frontend/vendors/css/vendor.bundle.addons.css">
+<link rel="stylesheet" href="<?php echo base_url('/assets/frontend/vendors/iconfonts/mdi/css/materialdesignicons.min.css')?>">
+<link rel="stylesheet" href="<?php echo base_url('/assets/frontend/vendors/iconfonts/ionicons/css/ionicons.css')?>">
+<link rel="stylesheet" href="<?php echo base_url('/assets/frontend/vendors/iconfonts/typicons/src/font/typicons.css')?>">
+<link rel="stylesheet" href="<?php echo base_url('/assets/frontend/vendors/iconfonts/flag-icon-css/css/flag-icon.min.css')?>">
+<link rel="stylesheet" href="<?php echo base_url('/assets/frontend/vendors/css/vendor.bundle.base.css')?>">
+<link rel="stylesheet" href="<?php echo base_url('/assets/frontend/vendors/css/vendor.bundle.addons.css')?>">
 <!-- endinject -->
 <!-- plugin css for this page -->
 <!-- End plugin css for this page -->
 <!-- inject:css -->
-<link rel="stylesheet" href="<?php echo base_url()?>assets/frontend/css/shared/style.css">
+<link rel="stylesheet" href="<?php echo base_url()?>/assets/frontend/css/shared/style.css">
 <!-- endinject -->
 <!-- Layout styles -->
-<link rel="stylesheet" href="<?php echo base_url()?>assets/frontend/css/demo_1/style.css">
+<link rel="stylesheet" href="<?php echo base_url()?>/assets/frontend/css/demo_1/style.css">
 <!-- End Layout styles -->
-<link rel="shortcut icon" href="<?php echo base_url()?>assets/frontend/images/favicon.png" />
+<link rel="shortcut icon" href="<?php echo base_url()?>/assets/frontend/images/favicon.png">
 <div class="container-scroller">
   <div class="container-fluid page-body-wrapper full-page-wrapper">
     <div class="content-wrapper d-flex align-items-center auth auth-bg-1 theme-one">
